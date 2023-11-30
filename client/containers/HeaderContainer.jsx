d1fbd915b4397f1d075e696421684856e81254d7
Tagi Magi <134989360+TagiMagi@users.noreply.github.com>
2023-11-29 18:59:01 -0600

    instantiate dark-theme

--- client/containers/HeaderContainer.jsx
+++ client/containers/HeaderContainer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import PopupForm from '../components/PopupForm.jsx';
 
 const HeaderContainer = () => {
+  function darkMode() {
+    document.body.classList.toggle('dark-theme');
+  }
   return (
     <div className='headerContainer'>
-      <div></div>
+      <button className='btn-toggle' onClick={darkMode}>
+        Dark-Mode
+      </button>
       <h1 id='header'>Job Application Tracker</h1>
       <PopupForm />
     </div>
