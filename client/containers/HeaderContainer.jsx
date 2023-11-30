88bc7941275736d41b792b378c8d1e3d021ac6cb
Tagi Magi <134989360+TagiMagi@users.noreply.github.com>
2023-11-30 09:58:12 -0600

    dark-mode styling update

--- client/containers/HeaderContainer.jsx
+++ client/containers/HeaderContainer.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 import PopupForm from '../components/PopupForm.jsx';
 
 const HeaderContainer = () => {
-  function darkMode() {
-    document.body.classList.toggle('dark-theme');
-    if (!document.body.style.backgroundColor) {
-      document.body.style.backgroundColor = 'rgb(100, 110, 290)';
-    } else document.body.style.backgroundColor = null;
-  }
+  // function darkMode() {
+  //   document.body.classList.toggle('dark-theme');
+  //   if (!document.body.style.backgroundColor) {
+  //     document.body.style.backgroundColor = 'rgb(100, 110, 290)';
+  //     document.body.querySelector('.btn-toggle').innerHTML = 'Light-Mode';
+  //   } else document.body.style.backgroundColor = null;
+  //   document.body.querySelector('.btn-toggle').innerHTML = 'Dark-Mode';
+  // }
+  const [darkMode, setDarkMode] = useState('false');
+
   return (
     <div className='headerContainer'>
-      <button className='btn-toggle' onClick={darkMode}>
+      <button
+        className='btn-toggle'
+        onClick={() => {
+          if (darkMode === 'false') setDarkMode('true');
+          setDarkMode('false');
+        }}
+      >
         Dark-Mode
       </button>
       <h1 id='header'>Job Application Tracker</h1>
